d556b0dcf9ef919fa43bb25f70861f39af2a7cd2
silvio1000 <silviojmoser@hotmail.com>
2026-07-23 20:02:42 -0300

    Corrige exibicao do logo no Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Navbar.css';
-import logo from "../../assets/dev.png";
+import logo from "../../assets/dev.jpg";
 
 function Navbar() {
   const [menuAberto, setMenuAberto] = useState(false);
