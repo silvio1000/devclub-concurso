7ac7d83180cf6cd23726cfb56f3aceb8a3fdf1c7
silvio1000 <silviojmoser@hotmail.com>
2026-07-23 18:25:28 -0300

    Configura GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 // Habilita o suporte ao React durante o desenvolvimento e a geração final.
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/devclub-concurso/",
+});
